9a48856f04a3373b02161ccf05ca0377579693d1
Cristian <cristianduro@gmail.com>
2026-03-14 00:28:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  base: "/garden-makeover-v2/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
